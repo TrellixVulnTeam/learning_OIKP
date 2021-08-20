355aced8de9343ad90573059b00791e7202e4586
Migorithm <saka90030@gmail.com>
2021-08-20 18:27:39 +0900

    flask!

--- APIs/api05_flask.py
+++ APIs/api05_flask.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask , request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#configure db for Flask
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+
+#db variable comes after configuration
+db = SQLAlchemy(app)
+
+##from terminal, and join python program you can put
+#
+# from pythonfile_name import db
+# from pythonfile_name import Drink
+# db.create_all() will create the real database on the directory
+# drink = Drink(name= "xxx", description="ddd")
+
+
+# To add it to your table,
+# db.session.add(drink)
+# db.session.commit()
+
+#it can also be    ->   db.session.add(Drink(name="Cherry",descrption="Taste just great"))
+
+
+
 
 class Drink(db.Model):
     #attribute
@@ -8,6 +33,9 @@ class Drink(db.Model):
     name = db.Column(db.String(80),unique=True, nullable=False)
     description = db.Column(db.String(120))
     #max length , and uniqueness
+
+    def __repr__(self):
+        return f"{self.id} - {self.name} - {self.description}"
 
 
 @app.route('/')
@@ -17,5 +45,37 @@ def index():
 
 @app.route('/drinks')
 def get_drinks():
+    drinks = Drink.query.all()
+    #this is how you get cursor.
+    output = []
+    for drink in drinks:
+        drink_data = {'name':drink.name,'description':drink.description}
+        output.append(drink_data)
+    return {"drinks" : output}
 
-    return {"drinks" : "drink data"}
+
+# as you put the id of drink you put as primary key,
+# you can get an access to that info.
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return {"name":drink.name, "description":drink.description}
+
+
+
+#
+@app.route('/drinks',methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'],description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {"id":drink.id}
+
+@app.route('/drinks/<id>',methods=['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message":"yeet!@"}
