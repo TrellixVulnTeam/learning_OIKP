00fdb5f2b7b9e9263984474fe73311e3ae2f91ec
Migorithm <saka90030@gmail.com>
2021-09-17 22:10:53 +0900

    mongo query

---
+++ APIs/appii.py
@@ -0,0 +1,7 @@
+import requests
+
+endpoint = "http://apis.data.go.kr/1470000/MdcinGrnIdntfcInfoService/getMdcinGrnIdntfcInfoList?"
+key = "serviceKey=OpSodFlF21AwAlRXdK%2F%2BbwKkvQzXZJv3KPBqV3uF9FG6ZNEtJp5cpntJ2RVqXQ0z38MryhsVUa5OqhWOpteJEQ%3D%3D"
+
+"http://apis.data.go.kr/1470000/MdcinGrnIdntfcInfoService/getMdcinGrnIdntfcInfoList?serviceKey=OpSodFlF21AwAlRXdK%2F%2BbwKkvQzXZJv3KPBqV3uF9FG6ZNEtJp5cpntJ2RVqXQ0z38MryhsVUa5OqhWOpteJEQ%3D%3D&item_name=%EC%95%8C%EB%A0%88%EA%B8%B0%EC%82%B4%EC%A0%9510%EB%B0%80%EB%A6%AC%EA%B7%B8%EB%9E%8C(%ED%8E%98%EB%AF%B8%EB%A1%9C%EB%9D%BC%EC%8A%A4%ED%8A%B8%EC%B9%BC%EB%A5%A8)&entp_name=%ED%98%84%EB%8C%80%EC%95%BD%ED%92%88(%EC%A3%BC)&item_seq=200402488&img_regist_ts=20041222&pageNo=1&numOfRows=3&edi_code=642003260"
+requests.get()
